965206fef1303de41fb5514e835c8766406a4e1c
ker4ik13 <kekustop228@mail.ru>
2023-11-28 17:08:19 +0300

    test: тест метрики яндекс

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import HomePage from "@/pages/HomePage";
 import { renderSeo } from "@/shared/renderSeo";
+import { UserKitchenService } from "@/services/UserKitchenService";
 
 export const metadata: Metadata = renderSeo({
   title: "Кухни на заказ в Москве от производителя. Кухни с гарантией 10 лет",
@@ -10,6 +11,12 @@ export const metadata: Metadata = renderSeo({
     "Кухни на заказ, Кухни в Москве, Производство кухонь, Гарантия 10 лет, Индивидуальные размеры кухонь, Немецкая фурнитура, Австрийская фурнитура, Срок изготовления 10-20 дней, Бесплатный 3D проект кухни, Визуализация кухни, Замер кухни, Расчет стоимости кухни, Долговечные кухни, Кухни с гарантированным качеством, Бесплатная консультация по кухням, Изготовление кухонь под заказ, Кухонные решения, Кухни с доставкой, Эксклюзивные кухни, Профессиональные дизайнеры кухонь",
 });
 
-export default function Home() {
+const getKitchens = async () => {
+  const kitchens = await UserKitchenService.getMainKitchens();
+  return kitchens;
+};
+
+export default async function Home() {
+  const kitchens = await getKitchens();
   return <HomePage />;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { Provider } from "react-redux";
 import store from "@/store/store";
+import Script from "next/script";
 export default function RootLayout({
   children,
 }: {
@@ -9,6 +10,24 @@ export default function RootLayout({
 }) {
   return (
     <Provider store={store}>
+      <Script
+        id='metrikaScript'
+        dangerouslySetInnerHTML={{
+          __html: `
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(95709344, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`,
+        }}
+      />
       <html lang='ru'>{children}</html>
     </Provider>
   );
