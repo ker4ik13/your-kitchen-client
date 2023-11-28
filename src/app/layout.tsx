316d2fc8947ff23e4903e17fb4c4a9f7500b7d0a
ker4ik13 <kekustop228@mail.ru>
2023-11-28 17:24:57 +0300

    fix: тест запуск метрики

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
 import { Provider } from "react-redux";
 import store from "@/store/store";
+import Script from "next/script";
 export default function RootLayout({
   children,
 }: {
@@ -11,6 +12,25 @@ export default function RootLayout({
 }) {
   return (
     <Provider store={store}>
+      <Script
+        id='metrikaScript'
+        dangerouslySetInnerHTML={{
+          __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(95709344, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+   
+   `,
+        }}
+      />
       <html lang='ru'>{children}</html>
     </Provider>
   );
