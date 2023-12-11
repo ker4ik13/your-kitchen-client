888212384d4423a4021fb0c9c1cb8c75943e1a64
ker4ik13 <kekustop228@mail.ru>
2023-12-11 21:18:28 +0300

    подготовка к cherry pic

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
         <meta name='theme-color' content='#433A31' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta name='yandex-verification' content='8cb11033a13960f6' />
+        {/* <script defer src='https://af.click.ru/af.js?id=12257'></script> */}
         {/* TODO: не забывать про метрику */}
         <Script
           id='warn'
