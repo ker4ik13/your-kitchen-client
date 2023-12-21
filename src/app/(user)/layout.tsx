49454890aca6f5d2bf252e4958ab01022aa705b7
ker4ik13 <kekustop228@mail.ru>
2023-12-21 16:30:17 +0300

    fix seo

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -37,15 +37,15 @@ export default function RootLayout({
           name='google-site-verification'
           content='Nf5juItdjnxMIYOb4kOe_8QgiyDbS4fqFVU1tZsjo38'
         />
-        {/* <script defer src='https://af.click.ru/af.js?id=12257'></script> */}
+        <script defer src='https://af.click.ru/af.js?id=12257'></script>
         {/* TODO: не забывать про метрику */}
-        <Script
+        {/* <Script
           id='warn'
           dangerouslySetInnerHTML={{
             __html: `console.warn('Включи метрику перед коммитом');`,
           }}
-        />
-        {/* <Script
+        /> */}
+        <Script
           async
           src='https://www.googletagmanager.com/gtag/js?id=G-9PXGLTWQJK'
         ></Script>
@@ -68,7 +68,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-T7HCN6P');`,
           }}
-        /> */}
+        />
       </head>
       <body className='grid grid-rows-[auto_1fr_auto] grid-cols-1 min-h-screen font-gilroy'>
         <Nav />
