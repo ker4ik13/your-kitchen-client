75f071014bd70053f486316b3964ec3cbbda929f
ker4ik13 <kekustop228@mail.ru>
2023-12-22 21:46:25 +0300

    fix robots

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -4,7 +4,14 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: ["/", "/portfolio", "/articles", "/articles/*"],
+      allow: [
+        "/",
+        "/portfolio",
+        "/portfolio/",
+        "/articles",
+        "/articles/",
+        "/articles/*",
+      ],
       disallow: ["/admin/", "/*?*", "/portfolio/*?*"],
     },
     sitemap: "https://youkuhnya.ru/sitemap.xml",
