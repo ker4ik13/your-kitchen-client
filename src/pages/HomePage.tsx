4c33b492aba908d13a8c5afbf76930684809db82
ker4ik13 <kekustop228@mail.ru>
2023-12-25 23:19:35 +0300

    refactor: улучшение производительности

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,85 +1,89 @@
-"use client";
+'use client';
 
-import HelloScreen from "@/widgets/HelloScreen/HelloScreen";
-import SecondScreen from "@/widgets/SecondScreen/SecondScreen";
-import Correction from "@/widgets/Correction/Correction";
-import AllVariants from "@/widgets/AllVariants/AllVariants";
-import Kitchens from "@/widgets/Kitchens/Kitchens";
-import ModalVideo from "@/widgets/Modals/ModalVideo";
-import ThanksModal from "@/widgets/Modals/ThanksModal";
-import { useState } from "react";
-import PreviewPhotos from "@/widgets/PreviewPhotos/PreviewPhotos";
-import { Provider } from "react-redux";
-import store from "@/store/store";
-import dynamic from "next/dynamic";
-import MiniLoading from "@/shared/MiniLoading";
-import styles from "./HomePage.module.scss";
-import Reviews from "@/widgets/Reviews/Reviews";
-import Modal1 from "@/widgets/Modals/Modal1";
-import Modal2 from "@/widgets/Modals/Modal2";
-import { LeaveRequestBlock } from "@/shared/LeaveRequestBlock";
-import { LeaveRequestBlock2 } from "@/shared/LeaveRequestBlock2";
-import MainArticles from "@/widgets/MainArticles/MainArticles";
+import { LeaveRequestBlock } from '@/shared/LeaveRequestBlock';
+import { LeaveRequestBlock2 } from '@/shared/LeaveRequestBlock2';
+import MiniLoading from '@/shared/MiniLoading';
+import store from '@/store/store';
+import AllVariants from '@/widgets/AllVariants/AllVariants';
+import Correction from '@/widgets/Correction/Correction';
+import HelloScreen from '@/widgets/HelloScreen/HelloScreen';
+import Kitchens from '@/widgets/Kitchens/Kitchens';
+import MainArticles from '@/widgets/MainArticles/MainArticles';
+import PreviewPhotos from '@/widgets/PreviewPhotos/PreviewPhotos';
+import Results from '@/widgets/Results/Results';
+import Reviews from '@/widgets/Reviews/Reviews';
+import SecondScreen from '@/widgets/SecondScreen/SecondScreen';
+import WhatsNext from '@/widgets/WhatsNext/WhatsNext';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import { Provider } from 'react-redux';
+import styles from './HomePage.module.scss';
 
-const DynamicOurTeam = dynamic(() => import("@/widgets/OurTeam/OurTeam"), {
-  loading: () => <MiniLoading className={styles.loading} />,
+const DynamicOurTeam = dynamic(() => import('@/widgets/OurTeam/OurTeam'), {
+	loading: () => <MiniLoading className={styles.loading} />,
 });
-const DynamicWhatsNext = dynamic(
-  () => import("@/widgets/WhatsNext/WhatsNext"),
-  {
-    loading: () => <MiniLoading className={styles.loading} />,
-  },
+const DynamicModal1 = dynamic(() => import('@/widgets/Modals/Modal1'), {
+	loading: () => <MiniLoading className={styles.loading} />,
+});
+const DynamicModal2 = dynamic(() => import('@/widgets/Modals/Modal2'), {
+	loading: () => <MiniLoading className={styles.loading} />,
+});
+const DynamicModalVideo = dynamic(() => import('@/widgets/Modals/ModalVideo'), {
+	loading: () => <MiniLoading className={styles.loading} />,
+});
+const DynamicThanksModal = dynamic(
+	() => import('@/widgets/Modals/ThanksModal'),
+	{
+		loading: () => <MiniLoading className={styles.loading} />,
+	},
 );
-const DynamicResults = dynamic(() => import("@/widgets/Results/Results"), {
-  loading: () => <MiniLoading className={styles.loading} />,
-});
 
 const HomePage = () => {
-  const [isOpenFirst, setIsOpenFirst] = useState(false);
-  const [isOpenSecond, setIsOpenSecond] = useState(false);
-  const [isOpenCatalog, setIsOpenCatalog] = useState(false);
-  const [isOpenThanks, setIsOpenThanks] = useState(false);
-  const [isOpenVideo, setIsOpenVideo] = useState(false);
+	const [isOpenFirst, setIsOpenFirst] = useState(false);
+	const [isOpenSecond, setIsOpenSecond] = useState(false);
+	const [isOpenCatalog, setIsOpenCatalog] = useState(false);
+	const [isOpenThanks, setIsOpenThanks] = useState(false);
+	const [isOpenVideo, setIsOpenVideo] = useState(false);
 
-  return (
-    <Provider store={store}>
-      <HelloScreen setIsOpen={setIsOpenFirst} />
-      <SecondScreen />
-      <Correction setIsOpen={setIsOpenSecond} setIsOpenVideo={setIsOpenVideo} />
-      <AllVariants setIsOpen={setIsOpenFirst} />
-      <PreviewPhotos />
-      <Kitchens setIsOpen={setIsOpenCatalog} />
-      <Modal1
-        isOpen={isOpenFirst}
-        setIsOpen={setIsOpenFirst}
-        setIsOpenThanks={setIsOpenThanks}
-      />
-      <Modal1
-        isOpen={isOpenCatalog}
-        setIsOpen={setIsOpenCatalog}
-        setIsOpenThanks={setIsOpenThanks}
-        buttonText='Получить каталог'
-      />
-      <Modal2
-        isOpen={isOpenSecond}
-        setIsOpen={setIsOpenSecond}
-        setIsOpenThanks={setIsOpenThanks}
-      />
-      <Reviews />
-      <DynamicOurTeam />
-      <MainArticles />
-      <LeaveRequestBlock />
-      <DynamicWhatsNext />
-      <DynamicResults />
-      <LeaveRequestBlock2 />
-      {isOpenThanks && <ThanksModal setIsOpen={setIsOpenThanks} />}
-      <ModalVideo
-        isOpen={isOpenVideo}
-        setIsOpen={setIsOpenVideo}
-        videoUrl='https://rutube.ru/video/ff3d0a32ad30b9b0344f3717337e0e05'
-      />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<HelloScreen setIsOpen={setIsOpenFirst} />
+			<SecondScreen />
+			<Correction setIsOpen={setIsOpenSecond} setIsOpenVideo={setIsOpenVideo} />
+			<AllVariants setIsOpen={setIsOpenFirst} />
+			<PreviewPhotos />
+			<Kitchens setIsOpen={setIsOpenCatalog} />
+			<DynamicModal1
+				isOpen={isOpenFirst}
+				setIsOpen={setIsOpenFirst}
+				setIsOpenThanks={setIsOpenThanks}
+			/>
+			<DynamicModal1
+				isOpen={isOpenCatalog}
+				setIsOpen={setIsOpenCatalog}
+				setIsOpenThanks={setIsOpenThanks}
+				buttonText='Получить каталог'
+			/>
+			<DynamicModal2
+				isOpen={isOpenSecond}
+				setIsOpen={setIsOpenSecond}
+				setIsOpenThanks={setIsOpenThanks}
+			/>
+			<Reviews />
+			<DynamicOurTeam />
+			<MainArticles />
+			<LeaveRequestBlock />
+			<WhatsNext />
+			<Results />
+			<LeaveRequestBlock2 />
+			{isOpenThanks && <DynamicThanksModal setIsOpen={setIsOpenThanks} />}
+			<DynamicModalVideo
+				isOpen={isOpenVideo}
+				setIsOpen={setIsOpenVideo}
+				videoUrl='https://rutube.ru/video/ff3d0a32ad30b9b0344f3717337e0e05'
+			/>
+		</Provider>
+	);
 };
 
 export default HomePage;
--- src/widgets/Kitchen/Kitchen.tsx
+++ src/widgets/Kitchen/Kitchen.tsx
@@ -1,93 +1,101 @@
-import { IKitchen } from "@/types/IKitchen";
-import styles from "./Kitchen.module.scss";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
-// import Swiper and modules styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "@/shared/styles/swiper-my.css";
-import { useAppDispatch } from "@/store/hooks";
-import { setIsOpen, setKitchen } from "@/store/kitchens.slice";
+import '@/shared/styles/swiper-my.css';
+import { useAppDispatch } from '@/store/hooks';
+import { setIsOpen, setKitchen } from '@/store/kitchens.slice';
+import { IKitchen } from '@/types/IKitchen';
+import Image from 'next/image';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import styles from './Kitchen.module.scss';
 
 interface KitchenProps {
-  kitchen: IKitchen;
-  isPreview?: boolean;
+	kitchen: IKitchen;
+	isPreview?: boolean;
 }
 
 const Kitchen = ({ kitchen, isPreview }: KitchenProps) => {
-  const dispatch = useAppDispatch();
+	const dispatch = useAppDispatch();
 
-  const openPreview = () => {
-    if (isPreview) {
-      dispatch(setKitchen(kitchen));
-      dispatch(setIsOpen(true));
-      document.body.classList.add("overflow");
-    } else {
-      return;
-    }
-  };
-  return (
-    <>
-      <Swiper
-        className={styles.kitchen}
-        navigation={true}
-        pagination={{
-          enabled: true,
-          clickable: true,
-        }}
-        allowTouchMove={window.innerWidth > 768 ? false : true}
-        loop={true}
-        modules={[Navigation, Pagination]}
-        onClick={openPreview}
-        itemScope
-        itemType='https://schema.org/Product'
-      >
-        <div className={styles.content}>
-          <div className={styles.betweenWrapper}>
-            <p className={styles.name} itemProp='name'>
-              {kitchen.title}
-            </p>
-            <div className={styles.tags} itemProp='keywords'>
-              <div className={styles.tag}>
-                <p>{kitchen.style.label}</p>
-              </div>
-              {kitchen.type && (
-                <div className={styles.tag}>
-                  <p>{kitchen.type.label}</p>
-                </div>
-              )}
-            </div>
-          </div>
-          <p className={styles.description} itemProp='description'>
-            {kitchen.description}
-          </p>
-          <div className={styles.contentWrapper}>
-            <p className={styles.price}>
-              <span className={styles.brown}>От </span>
-              {kitchen.price.toLocaleString("ru")}
-              <span>₽</span>
-            </p>
-            <div className={styles.price}>
-              <span className={styles.brown}>Срок </span>
-              {kitchen.term}
-            </div>
-          </div>
-        </div>
-        {kitchen.photos.map((photo, index) => (
-          <SwiperSlide key={index} className={styles.image}>
-            <img
-              src={photo}
-              className={styles.img}
-              draggable={false}
-              alt={`${kitchen.title}`}
-              itemProp='image'
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
-  );
+	const openPreview = () => {
+		if (isPreview) {
+			dispatch(setKitchen(kitchen));
+			dispatch(setIsOpen(true));
+			document.body.classList.add('overflow');
+		} else {
+			return;
+		}
+	};
+	return (
+		<>
+			<Swiper
+				className={styles.kitchen}
+				navigation={true}
+				pagination={{
+					enabled: true,
+					clickable: true,
+				}}
+				allowTouchMove={window.innerWidth > 768 ? false : true}
+				loop={true}
+				modules={[Navigation, Pagination]}
+				onClick={openPreview}
+				itemScope
+				itemType='https://schema.org/Product'
+			>
+				<div className={styles.content}>
+					<div className={styles.betweenWrapper}>
+						<p className={styles.name} itemProp='name'>
+							{kitchen.title}
+						</p>
+						<div className={styles.tags} itemProp='keywords'>
+							<div className={styles.tag}>
+								<p>{kitchen.style.label}</p>
+							</div>
+							{kitchen.type && (
+								<div className={styles.tag}>
+									<p>{kitchen.type.label}</p>
+								</div>
+							)}
+						</div>
+					</div>
+					<p className={styles.description} itemProp='description'>
+						{kitchen.description}
+					</p>
+					<div className={styles.contentWrapper}>
+						<p className={styles.price} itemProp='offers'>
+							<span
+								className={styles.brown}
+								itemProp='priceCurrency'
+								content='RUB'
+							>
+								От{' '}
+							</span>
+							{kitchen.price.toLocaleString('ru')}
+							<span>₽</span>
+						</p>
+						<div className={styles.price}>
+							<span className={styles.brown}>Срок </span>
+							{kitchen.term}
+						</div>
+					</div>
+				</div>
+				{kitchen.photos.map((photo, index) => (
+					<SwiperSlide key={index} className={styles.image}>
+						<Image
+							src={photo || ''}
+							className={styles.img}
+							draggable={false}
+							alt={`${kitchen.title}` || ''}
+							itemProp='image'
+							width={461}
+							height={500}
+						/>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</>
+	);
 };
 
 export default Kitchen;
