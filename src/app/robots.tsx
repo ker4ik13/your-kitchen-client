fd83d9e33672594cc220cc931b66d13a6b80dc32
ker4ik13 <kekustop228@mail.ru>
2024-02-02 16:11:17 +0300

    fix: исправлено сео

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -1,29 +1,32 @@
-import { CLIENT_URL, pagesLinks } from '@/shared/constants';
-import { MetadataRoute } from 'next';
+import { CLIENT_URL, pagesLinks } from "@/shared/constants";
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-	return {
-		rules: {
-			userAgent: '*',
-			allow: [
-				pagesLinks.main,
-				pagesLinks.portfolio,
-				`${pagesLinks.portfolio}/`,
-				pagesLinks.articles,
-				`${pagesLinks.articles}/`,
-				`${pagesLinks.articles}/*`,
-				pagesLinks.advantages,
-				pagesLinks.furniture,
-				`${pagesLinks.advantages}/`,
-			],
-			disallow: [
-				`${pagesLinks.admin}/`,
-				'/*?*',
-				`${pagesLinks.portfolio}/*?*`,
-				`${pagesLinks.advantages}/*?*`,
-				`${pagesLinks.furniture}/*?*`,
-			],
-		},
-		sitemap: `${CLIENT_URL}/sitemap.xml`,
-	};
+  return {
+    rules: {
+      userAgent: "*",
+      allow: [
+        pagesLinks.main,
+        pagesLinks.portfolio,
+        `${pagesLinks.portfolio}/`,
+        pagesLinks.articles,
+        `${pagesLinks.articles}/`,
+        `${pagesLinks.articles}/*`,
+        pagesLinks.advantages,
+        `${pagesLinks.advantages}/`,
+        pagesLinks.furniture,
+        `${pagesLinks.furniture}/`,
+        pagesLinks.discounts,
+        `${pagesLinks.discounts}/`,
+      ],
+      disallow: [
+        `${pagesLinks.admin}/`,
+        "/*?*",
+        `${pagesLinks.portfolio}/*?*`,
+        `${pagesLinks.advantages}/*?*`,
+        `${pagesLinks.furniture}/*?*`,
+      ],
+    },
+    sitemap: `${CLIENT_URL}/sitemap.xml`,
+  };
 }
--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,72 +1,76 @@
-import UserArticleService from '@/services/UserArticleService';
-import { CLIENT_URL, pagesData, pagesLinks } from '@/shared/constants';
+import UserArticleService from "@/services/UserArticleService";
+import { CLIENT_URL, pagesData, pagesLinks } from "@/shared/constants";
 
 const siteRoutes = [
-	{
-		link: '',
-		lastModify: '2024-01-25T17:16:55.776Z',
-	},
-	{
-		link: pagesLinks.portfolio,
-		lastModify: '2024-01-25T17:16:55.776Z',
-	},
-	{
-		link: pagesLinks.articles,
-		lastModify: '2024-01-25T17:16:55.776Z',
-	},
-	{
-		link: pagesLinks.advantages,
-		lastModify: new Date(2024, 0, 13, 16, 37).toISOString(),
-	},
-	{
-		link: pagesLinks.furniture,
-		lastModify: '2024-01-25T17:16:55.776Z',
-	},
+  {
+    link: "",
+    lastModify: "2024-01-25T17:16:55.776Z",
+  },
+  {
+    link: pagesLinks.portfolio,
+    lastModify: "2024-01-25T17:16:55.776Z",
+  },
+  {
+    link: pagesLinks.articles,
+    lastModify: "2024-01-25T17:16:55.776Z",
+  },
+  {
+    link: pagesLinks.advantages,
+    lastModify: new Date(2024, 0, 13, 16, 37).toISOString(),
+  },
+  {
+    link: pagesLinks.furniture,
+    lastModify: "2024-01-25T17:16:55.776Z",
+  },
+  {
+    link: pagesLinks.discounts,
+    lastModify: "2024-02-02T13:08:31.056Z",
+  },
 ];
 
 // Получение информции о статьях
 const getArticlesInfo = async () => {
-	const articles = await UserArticleService.getArticles();
-	const articlesLinks = articles.map((article) => {
-		return {
-			link: article.link,
-			lastModify: article.updatedAt || article.createdAt,
-		};
-	});
+  const articles = await UserArticleService.getArticles();
+  const articlesLinks = articles.map((article) => {
+    return {
+      link: article.link,
+      lastModify: article.updatedAt || article.createdAt,
+    };
+  });
 
-	// Последнее изменение страницы статей
-	const lastArticlesUpdated = articlesLinks.reduce((acc, article) => {
-		return new Date(article.lastModify).getTime() > new Date(acc).getTime()
-			? new Date(article.lastModify).getTime()
-			: new Date(acc).getTime();
-	}, new Date(articlesLinks[0].lastModify).getTime());
+  // Последнее изменение страницы статей
+  const lastArticlesUpdated = articlesLinks.reduce((acc, article) => {
+    return new Date(article.lastModify).getTime() > new Date(acc).getTime()
+      ? new Date(article.lastModify).getTime()
+      : new Date(acc).getTime();
+  }, new Date(articlesLinks[0].lastModify).getTime());
 
-	const articlesPageIndex = siteRoutes.findIndex(
-		(route) => route.link === pagesLinks.articles,
-	);
+  const articlesPageIndex = siteRoutes.findIndex(
+    (route) => route.link === pagesLinks.articles,
+  );
 
-	siteRoutes[articlesPageIndex].lastModify = new Date(
-		lastArticlesUpdated,
-	).toISOString();
+  siteRoutes[articlesPageIndex].lastModify = new Date(
+    lastArticlesUpdated,
+  ).toISOString();
 
-	return articlesLinks;
+  return articlesLinks;
 };
 
 export default async function sitemap() {
-	const articlesLinks = await getArticlesInfo();
+  const articlesLinks = await getArticlesInfo();
 
-	// Все страницы
-	const routes = siteRoutes.map((route) => ({
-		url: `${CLIENT_URL}${route.link}`,
-		lastModified: route.lastModify || new Date().toISOString(),
-		priority: 1.0,
-	}));
+  // Все страницы
+  const routes = siteRoutes.map((route) => ({
+    url: `${CLIENT_URL}${route.link}`,
+    lastModified: route.lastModify || new Date().toISOString(),
+    priority: 1.0,
+  }));
 
-	// Статьи
-	const articles = articlesLinks.map((article) => ({
-		url: `${CLIENT_URL}/${pagesData.articles.name}/${article.link}`,
-		lastModified: article.lastModify,
-	}));
+  // Статьи
+  const articles = articlesLinks.map((article) => ({
+    url: `${CLIENT_URL}/${pagesData.articles.name}/${article.link}`,
+    lastModified: article.lastModify,
+  }));
 
-	return [...routes, ...articles];
+  return [...routes, ...articles];
 }
