51fc9d3da8e3c834566f2768a33f9cd2461265de
ker4ik13 <kekustop228@mail.ru>
2024-03-01 19:36:53 +0300

    feat: добавлен экран на слайдер к 8 марта

--- src/widgets/ChoiseHelloScreen/KitchensHelloScreen.tsx
+++ src/widgets/ChoiseHelloScreen/KitchensHelloScreen.tsx
@@ -1,14 +1,33 @@
 import bgImage from "@/data/images/bg_kitchen_screen.jpg";
 import { OrangeButton } from "@/shared/ui";
-import Image from "next/image";
+import Image, { type StaticImageData } from "next/image";
 import styles from "./ChoiseHelloScreen.module.scss";
 
-export const KitchensHelloScreen = () => {
+interface ScreenProps {
+  image?: StaticImageData;
+  tags?: string[];
+  title?: string;
+  description?: string;
+  button?: {
+    href: string;
+    text: string;
+  };
+  titleLength?: "980";
+}
+
+export const KitchensHelloScreen = ({
+  button,
+  image,
+  tags,
+  title,
+  description,
+  titleLength,
+}: ScreenProps) => {
   return (
     <>
       <div className={`${styles.helloScreen} ${styles.kitchensScreen}`}>
         <Image
-          src={bgImage}
+          src={image ? image : bgImage}
           className={styles.bgImage}
           alt="Фон"
           draggable={false}
@@ -19,33 +38,54 @@ export const KitchensHelloScreen = () => {
           <div
             className={`${styles.tags} ${styles.column} ${styles.upperTags}`}
           >
-            <div className={styles.tagWrapper}>
-              <p className={styles.tag}>Рассрочка до 24 месяцев</p>
-              <div className={styles.circle}></div>
-            </div>
-            <div className={styles.tagWrapper}>
-              <p className={styles.tag}>Производство от 10 дней</p>
-              <div className={styles.circle}></div>
-            </div>
-            <div className={styles.tagWrapper}>
-              <p className={styles.tag}>Бесплатный дизайн проект</p>
-              <div className={`${styles.circle} ${styles.last}`}></div>
-            </div>
+            {tags &&
+              tags.map((tag, index) => (
+                <div className={styles.tagWrapper} key={index}>
+                  <p className={styles.tag}>{tag}</p>
+                  <div
+                    className={`${styles.circle} ${
+                      tags.length === index + 1 && styles.last
+                    }`}
+                  ></div>
+                </div>
+              ))}
+            {!tags && (
+              <>
+                <div className={styles.tagWrapper}>
+                  <p className={styles.tag}>Рассрочка до 24 месяцев</p>
+                  <div className={styles.circle}></div>
+                </div>
+                <div className={styles.tagWrapper}>
+                  <p className={styles.tag}>Производство от 10 дней</p>
+                  <div className={styles.circle}></div>
+                </div>
+                <div className={styles.tagWrapper}>
+                  <p className={styles.tag}>Бесплатный дизайн проект</p>
+                  <div className={`${styles.circle} ${styles.last}`}></div>
+                </div>
+              </>
+            )}
           </div>
           <h1
-            className={`${styles.title} ${styles.boldTitle} ${styles.w840} ${styles.ptHigh}`}
+            className={`${styles.title} ${styles.boldTitle} ${styles.w840} ${
+              styles.ptHigh
+            } ${titleLength && titleLength === "980" && styles.w980}`}
           >
-            Кухни на заказ от производителя в Москве и Московской области
+            {title
+              ? title
+              : "Кухни на заказ от производителя в Москве и Московской области"}
           </h1>
           <p className={`${styles.subtitle} ${styles.upperSubtitle}`}>
-            Индивидуальные кухни на заказ от производителя в Москве и Московской
-            области с гарантией и возможностью рассрочки до 24 месяцев:
-            выбирайте из широкого ассортимента стилей и материалов,
-            наслаждайтесь уникальным дизайном и комфортом.
+            {description
+              ? description
+              : "Индивидуальные кухни на заказ от производителя в Москве и Московской области с гарантией и возможностью рассрочки до 24 месяцев: выбирайте из широкого ассортимента стилей и материалов, наслаждайтесь уникальным дизайном и комфортом."}
           </p>
 
-          <OrangeButton className={styles.button} href="#kitchens">
-            Подробнее
+          <OrangeButton
+            className={styles.button}
+            href={button && button.href ? button.href : "kitchens"}
+          >
+            {button && button.text ? button.text : "Подробнее"}
           </OrangeButton>
         </div>
       </div>
